908ccf2ac1ba9a48ca3b31a2c267e066eb41e091
Mário Lucas <mariolucasdev@gmail.com>
2022-09-05 22:09:57 -0300

    Aula 3

---
+++ aula-03-operadores/operadores.py
@@ -0,0 +1,37 @@
+# > OPERAÇÕES MATEMÁTICAS (ARITIMÉTICAS)
+
+"""
+    - Soma: +
+    - Subtração: -
+    - Multiplicação: *
+    - Divisão: /
+    - Divisão Inteira: //
+    - Resto da Divisão: %
+    - Potência: **
+"""
+
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(numero1 % numero2)
+print(numero1 ** numero2)
+
+# > OPERAÇÕES BOOLEANOS
+
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2)          # False
+print(idade1 <= idade2)         # True
+print('python' == 'Python')     # False
+print('banana' != 'abacaxi')    # True
+print(altura1 >= altura2)       # True
+print(altura2 > altura3)        # False
